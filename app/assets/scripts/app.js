de0e0fc00b144e2ec07a6581063bd292bcaa2d68
Amaan Jambura <amaanjambura786@gmail.com>
2020-11-23 21:26:59 +0530

    Initial Commit

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,6 @@
+import "../styles/styles.css";
+//alert("Hello World from medservice!");
+if(module.hot) {
+    module.hot.accept();
+}
+console.log("This goes to console");
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+];
+//hot module replacement
+module.exports = {
+    entry: './app/assets/scripts/app.js',
+    output: {
+        filename: 'app.bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.resolve(__dirname, 'app'),
+        port: 4040,
+        hot: true, // 'hot module' feature allows injecting css and js refresh
+        //hot module replacement: run time pe jo chiz change hua vo us time change karna
+        host: '0.0.0.0'
+        //open: true //..to open the website automatically
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader?url=false',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: postCSSPlugins
+                            }
+                        }
+                    }
+                ],
+            },
+        ],
+    }
+}
