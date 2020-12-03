95e6df046c5c5b3f1c729bc1cd5e43163bd84499
Amaan Jambura <amaanjambura786@gmail.com>
2020-12-03 11:16:04 +0530

    Completed reveal on scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,16 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import $ from 'jquery';
 
 //Handles Mobile Menu/Header
 let mobileMenu = new MobileMenu();
 
+//Handles Reveal On Scroll 
+new RevealOnScroll($("#our-beginning"));
+new RevealOnScroll($("#departments"));
+new RevealOnScroll($("#counters"));
+new RevealOnScroll($("#testimonials"));
 
 if(module.hot) {
     module.hot.accept();
